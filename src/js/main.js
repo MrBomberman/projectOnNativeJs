305af19e68e843d7f707ba6232f7b8003d8ef8ab
Kirill <mrbomberman99@gmail.com>
2021-03-22 19:29:54 +0300

    added functions for modalWindows

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 import "./slider";
 import modalWindow from './modules/modalWindow'
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => { // назначаем глобальный обработчик событий - наши скрипты будут выполняться, когда наша дом структура на старнцие готова
     modalWindow();
   });
 
--- src/js/modules/modalWindow.js
+++ src/js/modules/modalWindow.js
@@ -1,35 +1,56 @@
 function modalWindow() {
+    function addClassesForModal(btnSelector, modalSelector, closeModalSelector) {
 
-    const btnCallMeasurer = document.querySelector('.popup_engineer_btn')
-    const modal = document.querySelector('.popup_engineer')
+        const btnCall = document.querySelectorAll(btnSelector);
+        const modal = document.querySelector(modalSelector);
 
-    function closeModal() {
-        modal.classList.add('hide');
-        modal.classList.remove('show');
-        document.body.style.overflow = '';
+        btnCall.forEach(btn => {
+            btn.addEventListener('click', () => { // на несколько одинаковых элементов вешаем одну функцию
+                openModal();
+            })
+        })
+
+        function openModal() {
+            modal.classList.add('show');
+            modal.classList.remove('hide');
+            document.body.style.overflow = 'hidden'; // чтобы скролилось только модальное окно, а не весь сайт
+        }
+    
+        function closeModal() {
+            modal.classList.add('hide');
+            modal.classList.remove('show');
+            document.body.style.overflow = '';
+        }
+    
+    
+    
+        modal.addEventListener('click', (e) => { // обязательно следует передать событие
+            if (e.target === modal || e.target.matches(closeModalSelector) == '') { // e.taget - то , куда кликнул пользователь, отслеживает
+            // если элемент, куда мы кликнули строго равен элементу, который мы показывали - то мы его будем закрывать
+            // когда мы кликаем вне окна - объект события и есть все модальное окно, но как только мы кликаем во внутрь модального окна - здесь e.target будет уже один из элементов внутри модального окна, а не оно само
+                closeModal();
+            }
+        });
+    
+        document.addEventListener('keydown', (e) => {
+            if (e.code === 'Escape' && modal.classList.contains('show')) { // и если модальное окно открыто
+                closeModal();
+            } // отслеживает код кнопки на клавиатуре, которую мы нажимаем
+        });
+    
+    }
+
+    function showModalByTime(selector, time){
+        setTimeout(() => {
+            document.querySelector(selector).classList.add('show');
+            document.body.style.overflow = 'hidden';
+        }, time)
     }
 
 
-    btnCallMeasurer.addEventListener('click', () => {
-        modal.classList.add('show');
-        modal.classList.remove('hide');
-        document.body.style.overflow = 'hidden';
-    })
-
-    modal.addEventListener('click', (e) => { // обязательно следует передать событие
-        if (e.target === modal || e.target.matches('.popup_engineer .popup_close') =='') { // e.taget - то , куда кликнул пользователь, отслеживает
-           closeModal();
-        }
-    });
-
-    document.addEventListener('keydown', (e) => {
-        if (e.code === 'Escape' && modal.classList.contains('show')) { // и если модальное окно открыто
-            closeModal();
-        } // отслеживает код кнопки на клавиатуре, которую мы нажимаем
-    });
-
-
-
+    addClassesForModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close')
+    addClassesForModal('.phone_link','.popup', '.popup .popup_close' );
+    showModalByTime('.popup', 12000)
 }
 
 export default modalWindow;
